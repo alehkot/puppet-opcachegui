8e32ec548740d4e57550435115128fa5d7da113e
Aleh Kot <kot.oleg@gmail.com>
2015-07-13 08:57:10 +0300

    Fix manifest.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,29 @@
-class opcachegui(
-  $webroot_location = '/var/www'
-) {
+# == Class: opcachegui
+#
+# This class installs the opcachegui package along with the necessary configuration.
+#
+# === Examples
+#
+#   class { 'opcachegui': }
+#
+# === Requirements
+#
+class opcachegui ( $settings ) {
 
-  $location = "$webroot_location/opcachegui"
-
-  exec { 'opcachegui-install':
-    command => "git clone https://github.com/PeeHaa/OpCacheGUI.git ${location}",
-    creates => "$location",
-    onlyif  => "test ! -d ${location}"
+  exec { 'exec mkdir -p /usr/share/php/opcachegui/source':
+    command => "mkdir -p /usr/share/php/opcachegui/source",
+    creates => "/usr/share/php/opcachegui/source",
   }
 
+  exec { 'php-download-opcachegui':
+    cwd     => "/tmp",
+    command => "git clone https://github.com/PeeHaa/OpCacheGUI.git opcachegui",
+    creates => "/usr/share/php/opcachegui/source/index.php",
+  }
+
+  exec { 'php-move-opcachegui':
+    command => "cp -r /tmp/opcachegui/* /usr/share/php/opcachegui/source",
+    creates => "/usr/share/php/opcachegui/source/index.php",
+    require => [ Exec["php-extract-opcachegui"], Exec["exec mkdir -p /usr/share/php/opcachegui/source"] ],
+  }
 }
